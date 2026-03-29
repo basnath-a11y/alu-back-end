543db05a9bca7819a6bab42c6a25e8d02ef74fb7
IREBE Asnath <b.asnath@alustudent.com>
2026-03-29 20:49:49 +0200

    Fix script: remove duplicates, fix imports, and correct function calls

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-Fetch employee TODO progress from API.
+Fetch TODO list progress for a given employee id from JSONPlaceholder API.
 """
-import requests
+import json
 import sys
+import urllib.request
 
 
 def fetch_json(url):
+	"""Fetch JSON data from a given URL."""
 	with urllib.request.urlopen(url) as resp:
 		return json.load(resp)
 
 
 def main():
+	"""Main function to display employee TODO progress."""
 	if len(sys.argv) != 2:
 		return
 	try:
@@ -29,38 +32,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
-#!/usr/bin/python3
-"""0-gather_data_from_an_API.py
-Fetch TODO list progress for a given employee id from JSONPlaceholder API.
-"""
-import json
-import sys
-import urllib.request
-
-
-def fetch_json(url):
-	with urllib.request.urlopen(url) as resp:
-		return json.load(resp)
-
-
-def main():
-	if len(sys.argv) != 2:
-		sys.exit(1)
-		return
-	try:
-		user_id = int(sys.argv[1])
-	except Exception:
-		return
-	user = fetch_json('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
-	todos = fetch_json('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
-	name = user.get('name')
-	completed = [t for t in todos if t.get('completed') is True]
-	print('Employee {} is done with tasks({}/{}):'.format(name, len(completed), len(todos)))
-	for task in completed:
-		print("\t {}".format(task.get("title")))
-
-
-if __name__ == "__main__":
-	fetch_todo(sys.argv[1])
 
